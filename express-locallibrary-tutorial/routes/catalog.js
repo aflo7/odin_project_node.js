32ab9235a718e10477a51f28f5ea937334fd19fb
Andres Flores <andresflo883@gmail.com>
2022-08-04 14:59:24 -0400

    added nav button functionality

--- express-locallibrary-tutorial/routes/catalog.js
+++ express-locallibrary-tutorial/routes/catalog.js
@@ -9,38 +9,73 @@ var BookInstance = require("../models/book")
 var Genre = require("../models/book")
 
 // every route in this file with start with '/catalog'
-router.get("/", function (req, res) {
-  res.send("catalog home page")
+
+// this page displays the number of Author, Book, BookInstance, and Genre in db
+router.get("/", function (req, res, next) {
+  Author.countDocuments((err, authorCount) => {
+    if (err) {
+      return console.error(err)
+    }
+
+    Book.countDocuments((err, bookCount) => {
+      if (err) {
+        return console.error(err)
+      }
+      BookInstance.countDocuments((err, bookInstanceCount) => {
+        if (err) {
+          return console.error(err)
+        }
+        Genre.countDocuments((err, genreCount) => {
+          if (err) {
+            return console.error(err)
+          }
+          return res.render("index", {
+            authorCount: authorCount,
+            bookCount: bookCount,
+            bookInstanceCount: bookInstanceCount,
+            genreCount: genreCount
+          })
+        })
+      })
+    })
+  })
 })
 
 router.get("/book", (req, res) => {
   // made code easier to read
   Book.find(function (err, result) {
-    if (err) return console.error(err)
+    if (err) {
+      return console.error(err)
+    }
 
     return res.send(result)
   })
 })
 
 router.get("/author", (req, res) => {
-  Author.find((err, result) => {
-    if (err) return console.error(err)
+  Author.find(function (err, result) {
+    if (err) {
+      return console.error(err)
+    }
 
     return res.send(result)
   })
 })
 
-router.get("/bookinstance", (req, res) => {
+router.get("/bookInstance", (req, res) => {
   BookInstance.find((err, result) => {
-    if (err) return console.error(err)
-
+    if (err) {
+      return console.error(err)
+    }
     return res.send(result)
   })
 })
 
 router.get("/genre", (req, res) => {
   Genre.find((err, result) => {
-    if (err) return console.error(err)
+    if (err) {
+      return console.error(err)
+    }
 
     return res.send(result)
   })
@@ -50,4 +85,8 @@ router.get("/:object/:id", function (req, res) {
   res.send("/:object/:id")
 })
 
+router.get("/book/:id", (req, res) => {})
+router.post("/book/create", (req, res) => {})
+
+router.delete("/book/delete/:id", (req, res) => {})
 module.exports = router
